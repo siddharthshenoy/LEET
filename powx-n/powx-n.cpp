05c9b9dfb4378a019eca2acd3ef1e52fd6fcd077
Siddharth Shenoy <56694590+siddharthshenoy@users.noreply.github.com>
2021-06-16 13:50:25 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (79.80%) - LeetHub

---
+++ powx-n/powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double a =1;
+        if(n>=0)
+        {
+            while(n!=0)
+            {
+            if ( n&1==1)
+             a = a*x;
+            x= x*x;
+            n= n/2;
+            }
+        }
+        else
+        {   while(n!=0)
+            {
+            if ( n&1==1)
+             a = a/x;
+            x= x*x;
+            n= n/2;
+            }
+        }
+        return a;
+    }
+};
